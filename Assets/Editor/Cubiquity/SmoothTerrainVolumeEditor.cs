eb5e2b11f7cdd6b328fa4157c3a941a8fd27ec3a
David Williams <david@volumesoffun.com>
2013-09-01 08:28:45 +0200

    Added sliders to smooth terrain editor.

--- Assets/Editor/Cubiquity/SmoothTerrainVolumeEditor.cs
+++ Assets/Editor/Cubiquity/SmoothTerrainVolumeEditor.cs
@@ -6,6 +6,9 @@ using System.Collections;
 public class SmoothTerrainVolumeEditor : Editor
 {
 	SmoothTerrainVolume smoothTerrainVolume;
+	
+	private float brushSize;
+	private float opacity;
 
 	public void OnEnable()
 	{
@@ -13,7 +16,16 @@ public class SmoothTerrainVolumeEditor : Editor
 	}
 	
 	public override void OnInspectorGUI()
-	{		
+	{
+		EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("Brush Size:", GUILayout.Width(80));
+			brushSize = GUILayout.HorizontalSlider(brushSize, 0.0f, 10.0f);
+		EditorGUILayout.EndHorizontal();
+		
+		EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("Opacity:", GUILayout.Width(80));
+			opacity = GUILayout.HorizontalSlider(opacity, 0.0f, 1.0f);
+		EditorGUILayout.EndHorizontal();
 	}
 	
 	public void OnSceneGUI()
@@ -34,7 +46,7 @@ public class SmoothTerrainVolumeEditor : Editor
 			{
 				//smoothTerrainVolume.SetVoxel(resultX, resultY, resultZ, paintColor);
 				Debug.Log ("Hit! - " + new Vector3(resultX, resultY, resultZ));
-				Cubiquity.SculptSmoothTerrainVolume(smoothTerrainVolume, resultX, resultY, resultZ, 5.0f, 1.0f);
+				Cubiquity.SculptSmoothTerrainVolume(smoothTerrainVolume, resultX, resultY, resultZ, brushSize, opacity);
 			}
 			
 			//Selection.activeGameObject = coloredCubesVolume.gameObject;
