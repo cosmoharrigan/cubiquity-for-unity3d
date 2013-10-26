e65fc52d953d32db6c2976e7d932a626a032577f
David Williams <david@volumesoffun.com>
2013-10-26 17:29:34 +0200

    Reordered some code.

--- Assets/Cubiquity/TerrainVolumeData.cs
+++ Assets/Cubiquity/TerrainVolumeData.cs
@@ -9,14 +9,6 @@ namespace Cubiquity
 	[System.Serializable]
 	public sealed class TerrainVolumeData : ScriptableObject
 	{		
-		[SerializeField]
-		private string pathToVoxels;
-		
-		// If set, this identifies the volume to the Cubiquity DLL. It can
-		// be tested against null to find if the volume is currently valid.
-		[System.NonSerialized] // Internal variables aren't serialized anyway?
-		internal uint? volumeHandle = null;
-		
 		// We need to explicitly serialize the private field because
 		// Unity3D doesn't automatically serialize the public property
 		[SerializeField]
@@ -28,6 +20,14 @@ namespace Cubiquity
 		
 		public TerrainMaterial[] materials;
 		
+		// If set, this identifies the volume to the Cubiquity DLL. It can
+		// be tested against null to find if the volume is currently valid.
+		[System.NonSerialized] // Internal variables aren't serialized anyway?
+		internal uint? volumeHandle = null;
+		
+		[SerializeField]
+		private string pathToVoxels;
+		
 		// Don't really like having this defined here. The base node size should be a rendering property rather than a
 		// property of the actual volume data. Need to make this change in the underlying Cubiquity library as well though.
 		private static uint DefaultBaseNodeSize = 32;
@@ -35,16 +35,6 @@ namespace Cubiquity
 		// We use a static Random for making filenames, as Randoms are seeded by timestamp
 		// and client code could potentially create a number of volumes on quick sucession.  
 		private static System.Random randomIntGenerator = new System.Random();
-		
-		public TerrainVolumeData()
-		{			
-			materials = new TerrainMaterial[License.MaxNoOfMaterials];
-			
-			for(int i = 0; i < materials.Length; i++)
-			{
-				materials[i] = new TerrainMaterial();
-			}
-		}
 		
 		// Ideally we will get rid of this function in the future. It is needed at the moment because the Region cannot be
 		// specified via the constructor as this class is created with ScriptableObject.CreateInstance(). However, Init() get
@@ -58,24 +48,6 @@ namespace Cubiquity
 			this.pathToVoxels = GeneratePathToVoxels();
 			
 			InitializeCubiquityVolume();
-		}
-		
-		void OnEnable()
-		{
-			InitializeCubiquityVolume();
-		}
-
-		internal void InitializeCubiquityVolume()
-		{	
-			// This function might get called multiple times. E.g the user might call it striaght after crating the volume (so
-			// they can add some initial data to the volume) and it might then get called again by OnEnable(). Handle this safely.
-
-			if((volumeHandle == null) && (_region != null))
-			{
-				// Create an empty region of the desired size.
-				volumeHandle = CubiquityDLL.NewTerrainVolume(region.lowerCorner.x, region.lowerCorner.y, region.lowerCorner.z,
-					region.upperCorner.x, region.upperCorner.y, region.upperCorner.z, pathToVoxels, DefaultBaseNodeSize, 0, 0);
-			}
 		}
 		
 		public byte GetVoxel(int x, int y, int z, uint materialIndex)
@@ -97,6 +69,37 @@ namespace Cubiquity
 				{
 					CubiquityDLL.SetVoxelMC(volumeHandle.Value, x, y, z, materialIndex, materialStrength);
 				}
+			}
+		}
+		
+		private void OnEnable()
+		{
+			if(materials == null)
+			{
+				materials = new TerrainMaterial[License.MaxNoOfMaterials];
+			}
+			
+			for(int i = 0; i < materials.Length; i++)
+			{
+				if(materials[i] == null)
+				{
+					materials[i] = new TerrainMaterial();
+				}
+			}
+			
+			InitializeCubiquityVolume();
+		}
+
+		private void InitializeCubiquityVolume()
+		{	
+			// This function might get called multiple times. E.g the user might call it striaght after crating the volume (so
+			// they can add some initial data to the volume) and it might then get called again by OnEnable(). Handle this safely.
+
+			if((volumeHandle == null) && (_region != null))
+			{
+				// Create an empty region of the desired size.
+				volumeHandle = CubiquityDLL.NewTerrainVolume(region.lowerCorner.x, region.lowerCorner.y, region.lowerCorner.z,
+					region.upperCorner.x, region.upperCorner.y, region.upperCorner.z, pathToVoxels, DefaultBaseNodeSize, 0, 0);
 			}
 		}
 		
