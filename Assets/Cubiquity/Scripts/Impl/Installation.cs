fce78d59d4d4a4fd23164419049dd37ded670deb
David Williams <david@volumesoffun.com>
2013-11-06 22:25:46 +0100

    Added error message for non-Windows platforms.

--- Assets/Cubiquity/Scripts/Impl/Installation.cs
+++ Assets/Cubiquity/Scripts/Impl/Installation.cs
@@ -9,6 +9,12 @@ namespace Cubiquity
 	{
 		public static void ValidateAndFix()
 		{
+			if( (Application.platform != RuntimePlatform.WindowsEditor) &&
+				(Application.platform != RuntimePlatform.WindowsPlayer) )
+			{
+				Debug.LogError("We're sorry, but Cubiquity for Unity3D is currently only supported on Windows. We hope to support more platfors in the future.");
+			}
+			
 			string fileName = "CubiquityC.dll";
 	        string sourcePath = System.IO.Path.Combine(Application.streamingAssetsPath, "Cubiquity");
 	        string destPath =  System.IO.Path.Combine(Application.dataPath, "..");
