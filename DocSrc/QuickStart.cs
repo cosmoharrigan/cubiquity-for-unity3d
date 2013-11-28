46525d21a98b66e4ffac355d804a471450b96f8e
David Williams <david@volumesoffun.com>
2013-11-28 16:27:16 +0100

    Work on quick start guide.

--- DocSrc/QuickStart.cs
+++ DocSrc/QuickStart.cs
@@ -15,5 +15,21 @@
  * \subsection secInstallFromGit Installing from Git
  *
  * Advanced users may be interested in using the latest development version of Cubiquity for Unity3D, rather than the stable releases. By doing this you may get access to functionality before it is officially released, but you can should expect that this is not yet fully tested. If you are interested in this then you can find our Git repository at https://bitbucket.org/volumesoffun/cubiquity-for-unity3d where further instructions are also provided.
+ *
+ * \section secFirstTerrainVolume Creating your first voxel terrain
+ *
+ * Cubiquity for Unity3D supports two types of voxel environments. We will begin by looking at the *Terrain Volume* which, as the name suggests, is intended for representing natural terrains. From a user point of view it is similar to Unity3D's built-in terrain, but additionally supports caves, overhangs, and flexible run-time editing.
+ *
+ * To create a Terrain Volume from within the unity3D editor you should go to the main menu and select `GameObject -> Create Other -> Terrain Volume`. The terrain volume will be created at the origin of your scene and should appear as shown in the image below:
  * 
+ * Picture here...
+ *
+ * Your new terrain should be automatically selected, and if you move your mouse cursor over it you should see a light-blue *brush marker* following your cursor position. This indicates where any terrain modification operations will be performed. You can click with the left mouse button to begin adding material into the terrain, thereby creating hills and ridges.
+ *
+ * Then the terrain is selected you will see the editing controls displayed in the Unity3D inspector panel (see picture below). These control the tools which you can use to modify the shape of your terrain or to apply materials to it. You can choose an tool to apply (sculpt, smooth, etc) by selecting one of the buttons at the top of the inspector, and then choose your desired brush options and/or materials. Left-clicking on the terrain will then apply the tool.
+ *
+ * Picture of tools here...
+ *
+ * Take some time to experiment with the editing tools which are available. After you have created a simple terrain you can also try importing one of the standard Unity character controllers so that you can walk around your terrain in play mode. Be aware that it can take a few seconds for the terrain to generate after you press the play button, so for this reason you should set your character to start a hundred units or so above the terrain. This way the terrain will have time to load before the character reaches ground level.
+ *
  */
