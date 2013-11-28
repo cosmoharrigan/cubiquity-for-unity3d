c7ec131000d7ab1774b5f925f92d17a3833f9722
David Williams <david@volumesoffun.com>
2013-11-28 21:02:46 +0100

    Work on principles of cubiquity document.

--- DocSrc/Principles.cs
+++ DocSrc/Principles.cs
@@ -10,5 +10,15 @@
  *
  * \section secCubiquity The Cubiquity Voxel Engine
  *
- * One of the first points to note is the difference between *Cubiquity* and *Cubiquity for Unity3D*. The first of these, *Cubiquity* is a native code (i.e. C/C++) library for storing, editing, and rendering voxel worlds. It is not tied to any particular game engine or platform, and can be used from a variety of languages. On the other hand, *Cubiquity for Unity3D* is a set of C# scripts which connect Cubiquity to the Unity3D. These scripts allow Unity3D games to create, edit and display Cubiquity volumes. As a user of Cubiquity for Unity3D you do not have access to the underlying C/C++ code of Cubiquity, but you do have a compiled version of the Cubiquity and the source code to the C# scripts.
+ * One of the first points to note is the difference between *Cubiquity* and *Cubiquity for Unity3D*. The first of these, *Cubiquity*, is a native code (i.e. C/C++) library for storing, editing, and rendering voxel worlds. It is not tied to any particular game engine or platform, and can be used from a variety of languages. On the other hand, *Cubiquity for Unity3D* is a set of C# scripts which connect Cubiquity to the Unity3D. These scripts allow Unity3D games to create, edit and display Cubiquity volumes. As a user of Cubiquity for Unity3D you do not have access to the underlying C/C++ code of Cubiquity, but you do have a compiled version of the Cubiquity and the source code to the C# scripts.
+ *
+ * The interaction between the underlying Cubiquity voxel engine and the Cubiquity for Unity3D integration layer is shown in the diagram below. Further information is provided after the diagram.
+ *
+ * Diagram here...
+ *
+ * \subsection secNativeCode The Cubiquity Native-Code Library
+ *
+ * Using native-code allows for some significant performance and memory optimizations compared to systems implemented using just Unity3D scripts. However, it also imposes some limitations, in particular that it cannot be used with the Unity3D web-player because this does not support native code. The native-code library also has to be compiled separately for each platform on which it needs to run (Windows, OSX, Linux, etc), but this is an issue which we as developers have to deal with rather than you as a user.
+ * 
+ * \subsection secVoxelDatabase The Cubiquity Voxel Database Format
  */
