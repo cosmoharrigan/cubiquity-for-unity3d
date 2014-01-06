a9d00e0de067c2250d2a1c73864380f9936f5d8f
David Williams <david@volumesoffun.com>
2014-01-06 23:39:38 +0100

    Octree is now discarded before saving scene.

---
+++ Assets/Cubiquity/Editor/DiscardOctreeOnSave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Cubiquity
+{
+	[ExecuteInEditMode]
+	public class DiscardOctreeOnSave : UnityEditor.AssetModificationProcessor
+	{
+	    public static void OnWillSaveAssets( string[] assets )
+	    {
+			Debug.Log("In OnWillSaveAssets()");
+			
+			foreach(Volume volume in Volume.all)
+			{
+				Debug.Log ("  Discarding Octree");
+				Object.DestroyImmediate(volume.rootGameObject);
+			}
+	    }
+	}
+}
