532f2b03208edc2662a5d08323545119a8496774
David Williams <david@volumesoffun.com>
2014-01-09 23:40:51 +0100

    Tidying up.

--- Assets/Cubiquity/Editor/DiscardOctreeOnPlay.cs
+++ Assets/Cubiquity/Editor/DiscardOctreeOnPlay.cs
@@ -3,29 +3,32 @@ using UnityEngine;
 
 namespace Cubiquity
 {
+	// This class is something of a hack. We don't want to save the octree which we build for a volume as we generate
+	// this on demand, but there appears to be a bug in Unity that even when the DontSave flag is set a reference to 
+	// the object is save even when the object itself is not. This causes errors when loading. So instead of setting
+	// the DontSave flag we instead tap into the serialisation code and discard the octree before saving occurs. This
+	// means both when really saving the scene to disk but also when simply switching between edit mode and play mode.
 	[InitializeOnLoad]
 	class DiscardOctreeOnPlay
 	{
 	    static DiscardOctreeOnPlay()
 	    {
+			// Catch the event which occurs when switching modes.
 	        EditorApplication.playmodeStateChanged += OnPlaymodeStateChanged;
 	    }
 	 
 	    static void OnPlaymodeStateChanged ()
 	    {
-			Debug.Log("OnPlaymodeStateChanged()");
+			// We only need to discard the octree in edit mode, beacause when leaving play mode serialization is not
+			// performed. This event occurs both when leaving the old mode and again when entering the new mode, but 
+			// when entering edit mode the root null should already be null and ddiscarding it again is harmless.
 			if(!EditorApplication.isPlaying)
 			{
-				//foreach(Volume volume in Volume.allEnabled)
 				Object[] volumes = Object.FindObjectsOfType(typeof(Volume));
 				foreach(Object volume in volumes)
 				{
-					Debug.Log("Deleting root node");
-					((Volume)volume).StopCoroutine("SynchronizationCoroutine");
 					Object.DestroyImmediate(((Volume)volume).rootGameObject);
 					((Volume)volume).rootGameObject = null;
-					//volume.syncOnUpdate = false;
-					//UpdateAllVolumes.syncVolumes = false;
 				}
 			}
 	    }
--- Assets/Cubiquity/Editor/DiscardOctreeOnSave.cs
+++ Assets/Cubiquity/Editor/DiscardOctreeOnSave.cs
@@ -3,18 +3,21 @@ using UnityEditor;
 
 namespace Cubiquity
 {
+	// This class is something of a hack. We don't want to save the octree which we build for a volume as we generate
+	// this on demand, but there appears to be a bug in Unity that even when the DontSave flag is set a reference to 
+	// the object is save even when the object itself is not. This causes errors when loading. So instead of setting
+	// the DontSave flag we instead tap into the serialisation code and discard the octree before saving occurs. This
+	// means both when really saving the scene to disk but also when simply switching between edit mode and play mode.
 	[ExecuteInEditMode]
 	public class DiscardOctreeOnSave : UnityEditor.AssetModificationProcessor
 	{
 	    public static void OnWillSaveAssets( string[] assets )
 	    {
-			Debug.Log("In OnWillSaveAssets()");
-			
 			Object[] volumes = Object.FindObjectsOfType(typeof(Volume));
 			foreach(Object volume in volumes)
 			{
-				Debug.Log ("  Discarding Octree");
 				Object.DestroyImmediate(((Volume)volume).rootGameObject);
+				((Volume)volume).rootGameObject = null;
 			}
 	    }
 	}
