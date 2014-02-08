1c29442f35ab2fb4d7c187a36e05312ebca19031
David Williams <david@volumesoffun.com>
2014-02-08 15:02:45 +0000

    Tidied up maze example. Required components are now part of the scene to avoid the problems with 'DontSave' which we've seen in the past.

--- Assets/Cubiquity/Examples/Maze From Image/ColoredCubeMazeFromImage.cs
+++ Assets/Cubiquity/Examples/Maze From Image/ColoredCubeMazeFromImage.cs
@@ -7,7 +7,7 @@ using Cubiquity;
 public class ColoredCubeMazeFromImage : MonoBehaviour
 {
 	// Use this for initialization
-	void OnEnable ()
+	void Start ()
 	{
 		// We will load our texture from the supplied maze image. If you wish to supply your own image then please
 		// note that in Unity 4 you have to set the 'Read/Write Enabled flag' in the texture import properties.
@@ -24,22 +24,11 @@ public class ColoredCubeMazeFromImage : MonoBehaviour
 		// Start with some empty volume data and we'll write our maze into this..
 		ColoredCubesVolumeData data = ColoredCubesVolumeData.CreateEmptyVolumeData(new Region(0, 0, 0, width-1, height-1, depth-1));
 		
-		// Now we take the TerrainVolumeData we have just created and build a TerrainVolume from it.
-		// We also name it and make it a child of the generator to keep things tidy, though this isn't required.
-		// Lastly, the 'DontSave' flag is set because we simply regenerate the volume when switching to play mode.
-		//GameObject maze = ColoredCubesVolume.CreateGameObject(data);
-		//maze.name = "Maze Volume";
-		//maze.transform.parent = transform;
-		//maze.hideFlags = HideFlags.DontSave;
-		
-		//Add the requied components.
-		ColoredCubesVolume coloredCubesVolume = gameObject.AddComponent<ColoredCubesVolume>();
-		gameObject.AddComponent<ColoredCubesVolumeRenderer>();
-		gameObject.AddComponent<ColoredCubesVolumeCollider>();
+		//Get the main volume component
+		ColoredCubesVolume coloredCubesVolume = gameObject.GetComponent<ColoredCubesVolume>();
 		
 		// Attactch the empty data we created previously
-		coloredCubesVolume.data = data;
-		
+		coloredCubesVolume.data = data;		
 		
 		// At this point we have a volume created and can now start writting our maze data into it.
 		
--- Assets/Cubiquity/Scripts/ColoredCubesVolume.cs
+++ Assets/Cubiquity/Scripts/ColoredCubesVolume.cs
@@ -41,20 +41,23 @@ namespace Cubiquity
 		// We shold try and fix this by using raycasting to check if a voxel is under the mouse cursor?
 		void OnDrawGizmos()
 		{
-			// Compute the size of the volume.
-			int width = (data.enclosingRegion.upperCorner.x - data.enclosingRegion.lowerCorner.x) + 1;
-			int height = (data.enclosingRegion.upperCorner.y - data.enclosingRegion.lowerCorner.y) + 1;
-			int depth = (data.enclosingRegion.upperCorner.z - data.enclosingRegion.lowerCorner.z) + 1;
-			float offsetX = width / 2;
-			float offsetY = height / 2;
-			float offsetZ = depth / 2;
-			
-			// The origin is at the centre of a voxel, but we want this box to start at the corner of the voxel.
-			Vector3 halfVoxelOffset = new Vector3(0.5f, 0.5f, 0.5f);
-			
-			// Draw an invisible box surrounding the olume. This is what actually gets picked.
-	        Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.0f);
-			Gizmos.DrawCube (transform.position - halfVoxelOffset + new Vector3(offsetX, offsetY, offsetZ), new Vector3 (width, height, depth));
+			if(data != null)
+			{
+				// Compute the size of the volume.
+				int width = (data.enclosingRegion.upperCorner.x - data.enclosingRegion.lowerCorner.x) + 1;
+				int height = (data.enclosingRegion.upperCorner.y - data.enclosingRegion.lowerCorner.y) + 1;
+				int depth = (data.enclosingRegion.upperCorner.z - data.enclosingRegion.lowerCorner.z) + 1;
+				float offsetX = width / 2;
+				float offsetY = height / 2;
+				float offsetZ = depth / 2;
+				
+				// The origin is at the centre of a voxel, but we want this box to start at the corner of the voxel.
+				Vector3 halfVoxelOffset = new Vector3(0.5f, 0.5f, 0.5f);
+				
+				// Draw an invisible box surrounding the olume. This is what actually gets picked.
+		        Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.0f);
+				Gizmos.DrawCube (transform.position - halfVoxelOffset + new Vector3(offsetX, offsetY, offsetZ), new Vector3 (width, height, depth));
+			}
 	    }
 		
 		public override void Synchronize()
