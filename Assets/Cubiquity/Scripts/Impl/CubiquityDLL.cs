b8bc8f1366043bb228c21ee9c62f070435ee3ee3
David Williams <david@volumesoffun.com>
2014-02-19 23:37:21 +0000

    Fixed paths to work on Linux.

--- Assets/Cubiquity/Scripts/Impl/CubiquityDLL.cs
+++ Assets/Cubiquity/Scripts/Impl/CubiquityDLL.cs
@@ -9,9 +9,9 @@ namespace Cubiquity
 	public class CubiquityDLL
 	{
 #if UNITY_EDITOR
-		public const string pathToCubiquitySDK = "Assets/StreamingAssets/Cubiquity";
+		private const string dllToImport = "Assets/StreamingAssets/Cubiquity/CubiquityC";
 #else
-		public const string pathToCubiquitySDK = ".";
+		private const string dllToImport = "CubiquityC";
 #endif
 			
 		// This static constructor is supposed to make sure that the Cubiquity.dll is in the right place before the DllImport is done.
@@ -33,7 +33,7 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Volume functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewEmptyColoredCubesVolume(int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ, StringBuilder datasetName, uint baseNodeSize, out uint result);
 		public static uint NewEmptyColoredCubesVolume(int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ, string datasetName, uint baseNodeSize)
 		{
@@ -42,7 +42,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewColoredCubesVolumeFromVDB(StringBuilder datasetName, uint baseNodeSize, out uint result);
 		public static uint NewColoredCubesVolumeFromVDB(string datasetName, uint baseNodeSize)
 		{
@@ -51,7 +51,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewColoredCubesVolumeFromVolDat(StringBuilder voldatFolder, StringBuilder datasetName, uint baseNodeSize, out uint result);	
 		public static uint NewColoredCubesVolumeFromVolDat(string voldatFolder, string datasetName, uint baseNodeSize)
 		{
@@ -60,7 +60,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewColoredCubesVolumeFromHeightmap(StringBuilder heightmapFileName, StringBuilder colormapFileName, StringBuilder datasetName, uint baseNodeSize, out uint result);	
 		public static uint NewColoredCubesVolumeFromHeightmap(string heightmapFileName, string colormapFileName, string datasetName, uint baseNodeSize)
 		{
@@ -69,49 +69,49 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuUpdateVolume(uint volumeHandle);
 		public static void UpdateVolume(uint volumeHandle)
 		{
 			Validate(cuUpdateVolume(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetEnclosingRegion(uint volumeHandle, out int lowerX, out int lowerY, out int lowerZ, out int upperX, out int upperY, out int upperZ);	
 		public static void GetEnclosingRegion(uint volumeHandle, out int lowerX, out int lowerY, out int lowerZ, out int upperX, out int upperY, out int upperZ)
 		{		
 			Validate(cuGetEnclosingRegion(volumeHandle, out lowerX, out lowerY, out lowerZ, out upperX, out upperY, out upperZ));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuSetVoxel(uint volumeHandle, int x, int y, int z, QuantizedColor color);
 		public static void SetVoxel(uint volumeHandle, int x, int y, int z, QuantizedColor color)
 		{
 			Validate(cuSetVoxel(volumeHandle, x, y, z, color));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetVoxel(uint volumeHandle, int x, int y, int z, out QuantizedColor color);	
 		public static void GetVoxel(uint volumeHandle, int x, int y, int z, out QuantizedColor color)
 		{		
 			Validate(cuGetVoxel(volumeHandle, x, y, z, out color));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuDeleteColoredCubesVolume(uint volumeHandle);
 		public static void DeleteColoredCubesVolume(uint volumeHandle)
 		{
 			Validate(cuDeleteColoredCubesVolume(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuAcceptOverrideBlocks(uint volumeHandle);
 		public static void AcceptOverrideBlocks(uint volumeHandle)
 		{
 			Validate(cuAcceptOverrideBlocks(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuDiscardOverrideBlocks(uint volumeHandle);
 		public static void DiscardOverrideBlocks(uint volumeHandle)
 		{
@@ -120,7 +120,7 @@ namespace Cubiquity
 		
 		//--------------------------------------------------------------------------------
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewEmptyTerrainVolume(int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ, StringBuilder datasetName, uint baseNodeSize, out uint result);
 		public static uint NewEmptyTerrainVolume(int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ, string datasetName, uint baseNodeSize)
 		{
@@ -129,7 +129,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNewTerrainVolumeFromVDB(StringBuilder datasetName, uint baseNodeSize, out uint result);
 		public static uint NewTerrainVolumeFromVDB(string datasetName, uint baseNodeSize)
 		{
@@ -138,42 +138,42 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuUpdateVolumeMC(uint volumeHandle);
 		public static void UpdateVolumeMC(uint volumeHandle)
 		{
 			Validate(cuUpdateVolumeMC(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetVoxelMC(uint volumeHandle, int x, int y, int z, out MaterialSet materialSet);	
 		public static void GetVoxelMC(uint volumeHandle, int x, int y, int z, out MaterialSet materialSet)
 		{		
 			Validate(cuGetVoxelMC(volumeHandle, x, y, z, out materialSet));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuSetVoxelMC(uint volumeHandle, int x, int y, int z, MaterialSet materialSet);
 		public static void SetVoxelMC(uint volumeHandle, int x, int y, int z, MaterialSet materialSet)
 		{
 			Validate(cuSetVoxelMC(volumeHandle, x, y, z, materialSet));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuDeleteTerrainVolume(uint volumeHandle);
 		public static void DeleteTerrainVolume(uint volumeHandle)
 		{
 			Validate(cuDeleteTerrainVolume(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuAcceptOverrideBlocksMC(uint volumeHandle);
 		public static void AcceptOverrideBlocksMC(uint volumeHandle)
 		{
 			Validate(cuAcceptOverrideBlocksMC(volumeHandle));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuDiscardOverrideBlocksMC(uint volumeHandle);
 		public static void DiscardOverrideBlocksMC(uint volumeHandle)
 		{
@@ -183,7 +183,7 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Octree functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuHasRootOctreeNode(uint volumeHandle, out uint result);
 		public static uint HasRootOctreeNode(uint volumeHandle)
 		{
@@ -192,7 +192,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetRootOctreeNode(uint volumeHandle, out uint result);
 		public static uint GetRootOctreeNode(uint volumeHandle)
 		{
@@ -201,7 +201,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuHasChildNode(uint nodeHandle, uint childX, uint childY, uint childZ, out uint result);
 		public static uint HasChildNode(uint nodeHandle, uint childX, uint childY, uint childZ)
 		{
@@ -210,7 +210,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetChildNode(uint nodeHandle, uint childX, uint childY, uint childZ, out uint result);
 		public static uint GetChildNode(uint nodeHandle, uint childX, uint childY, uint childZ)
 		{
@@ -219,7 +219,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNodeHasMesh(uint nodeHandle, out uint result);
 		public static uint NodeHasMesh(uint nodeHandle)
 		{
@@ -228,14 +228,14 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNodePosition(uint nodeHandle, out int x, out int y, out int z);
 		public static void GetNodePosition(uint nodeHandle, out int x, out int y, out int z)
 		{
 			Validate(cuGetNodePosition(nodeHandle, out x, out y, out z));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetMeshLastUpdated(uint nodeHandle, out uint result);
 		public static uint GetMeshLastUpdated(uint nodeHandle)
 		{
@@ -246,7 +246,7 @@ namespace Cubiquity
 		
 		//----------------------------------------------------------------------
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuHasRootOctreeNodeMC(uint volumeHandle, out uint result);
 		public static uint HasRootOctreeNodeMC(uint volumeHandle)
 		{
@@ -255,7 +255,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetRootOctreeNodeMC(uint volumeHandle, out uint result);
 		public static uint GetRootOctreeNodeMC(uint volumeHandle)
 		{
@@ -264,7 +264,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuHasChildNodeMC(uint nodeHandle, uint childX, uint childY, uint childZ, out uint result);
 		public static uint HasChildNodeMC(uint nodeHandle, uint childX, uint childY, uint childZ)
 		{
@@ -273,7 +273,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetChildNodeMC(uint nodeHandle, uint childX, uint childY, uint childZ, out uint result);
 		public static uint GetChildNodeMC(uint nodeHandle, uint childX, uint childY, uint childZ)
 		{
@@ -282,7 +282,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuNodeHasMeshMC(uint nodeHandle, out uint result);
 		public static uint NodeHasMeshMC(uint nodeHandle)
 		{
@@ -291,14 +291,14 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNodePositionMC(uint nodeHandle, out int x, out int y, out int z);
 		public static void GetNodePositionMC(uint nodeHandle, out int x, out int y, out int z)
 		{
 			Validate(cuGetNodePositionMC(nodeHandle, out x, out y, out z));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetMeshLastUpdatedMC(uint nodeHandle, out uint result);
 		public static uint GetMeshLastUpdatedMC(uint nodeHandle)
 		{
@@ -310,9 +310,9 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Mesh functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNoOfIndices(uint octreeNodeHandle, out uint result);
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetIndices(uint octreeNodeHandle, out int[] result);
 		public static int[] GetIndices(uint octreeNodeHandle)
 		{
@@ -325,9 +325,9 @@ namespace Cubiquity
 			return result;
 		}
 			
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNoOfVertices(uint octreeNodeHandle, out uint result);
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetVertices(uint octreeNodeHandle, out CubiquityVertex[] result);
 		public static CubiquityVertex[] GetVertices(uint octreeNodeHandle)
 		{
@@ -343,9 +343,9 @@ namespace Cubiquity
 		
 		//--------------------------------------------------------------------------------
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNoOfIndicesMC(uint octreeNodeHandle, out uint result);
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetIndicesMC(uint octreeNodeHandle, out int[] result);
 		public static int[] GetIndicesMC(uint octreeNodeHandle)
 		{
@@ -358,9 +358,9 @@ namespace Cubiquity
 			return result;
 		}
 			
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetNoOfVerticesMC(uint octreeNodeHandle, out uint result);
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetVerticesMC(uint octreeNodeHandle, out CubiquitySmoothVertex[] result);
 		public static CubiquitySmoothVertex[] GetVerticesMC(uint octreeNodeHandle)
 		{
@@ -377,7 +377,7 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Clock functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGetCurrentTime(out uint result);
 		public static uint GetCurrentTime()
 		{
@@ -389,7 +389,7 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Raycasting functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuPickFirstSolidVoxel(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out int resultX, out int resultY, out int resultZ, out uint result);
 		public static uint PickFirstSolidVoxel(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out int resultX, out int resultY, out int resultZ)
 		{
@@ -398,7 +398,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuPickLastEmptyVoxel(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out int resultX, out int resultY, out int resultZ, out uint result);
 		public static uint PickLastEmptyVoxel(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out int resultX, out int resultY, out int resultZ)
 		{
@@ -407,7 +407,7 @@ namespace Cubiquity
 			return result;
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuPickTerrainSurface(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out float resultX, out float resultY, out float resultZ, out uint result);
 		public static uint PickTerrainSurface(uint volumeHandle, float rayStartX, float rayStartY, float rayStartZ, float rayDirX, float rayDirY, float rayDirZ, out float resultX, out float resultY, out float resultZ)
 		{
@@ -420,28 +420,28 @@ namespace Cubiquity
 		// Editing functions
 		////////////////////////////////////////////////////////////////////////////////
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuSculptTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount);
 		public static void SculptTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount)
 		{
 			Validate(cuSculptTerrainVolume(volumeHandle, centerX, centerY, centerZ, brushInnerRadius, brushOuterRadius, amount));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuBlurTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount);
 		public static void BlurTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount)
 		{
 			Validate(cuBlurTerrainVolume(volumeHandle, centerX, centerY, centerZ, brushInnerRadius, brushOuterRadius, amount));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuBlurTerrainVolumeRegion(uint volumeHandle, int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ);
 		public static void BlurTerrainVolumeRegion(uint volumeHandle, int lowerX, int lowerY, int lowerZ, int upperX, int upperY, int upperZ)
 		{
 			Validate(cuBlurTerrainVolumeRegion(volumeHandle, lowerX, lowerY, lowerZ, upperX, upperY, upperZ));
 		}
 		
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuPaintTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount, uint materialIndex);
 		public static void PaintTerrainVolume(uint volumeHandle, float centerX, float centerY, float centerZ, float brushInnerRadius, float brushOuterRadius, float amount, uint materialIndex)
 		{
@@ -451,7 +451,7 @@ namespace Cubiquity
 		////////////////////////////////////////////////////////////////////////////////
 		// Volume generation functions
 		////////////////////////////////////////////////////////////////////////////////
-		[DllImport (pathToCubiquitySDK + "/CubiquityC")]
+		[DllImport (dllToImport)]
 		private static extern int cuGenerateFloor(uint volumeHandle, int lowerLayerHeight, uint lowerLayerMaterial, int upperLayerHeight, uint upperLayerMaterial);
 		public static void GenerateFloor(uint volumeHandle, int lowerLayerHeight, uint lowerLayerMaterial, int upperLayerHeight, uint upperLayerMaterial)
 		{
