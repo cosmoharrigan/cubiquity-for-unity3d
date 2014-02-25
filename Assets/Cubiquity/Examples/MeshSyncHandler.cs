f49dc7034d57d8e84cd191baf6263eb0444b995d
David Williams <david@volumesoffun.com>
2014-02-25 23:35:25 +0000

    Added example of handling mesh sync events.

---
+++ Assets/Cubiquity/Examples/MeshSyncHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Cubiquity;
+
+public class MeshSyncHandler : MonoBehaviour
+{
+	Volume volume;
+		
+	void OnEnable()
+	{
+		volume = gameObject.GetComponent<Volume>();
+		
+		if(volume !=  null)
+		{
+			volume.OnMeshSyncComplete += PrintSyncCompleteMessage;
+			volume.OnMeshSyncLost += PrintSyncLostMessage;
+		}
+		else
+		{
+			Debug.LogError("This example script should be attached to a game object with a TerrainVolume or ColoredCubesVolume attached");
+		}
+	}
+	
+	void OnDisable()
+	{
+		if(volume !=  null)
+		{
+			volume.OnMeshSyncComplete -= PrintSyncCompleteMessage;
+			volume.OnMeshSyncLost -= PrintSyncLostMessage;
+		}
+	}
+	
+	void PrintSyncCompleteMessage()
+	{
+		Debug.Log ("Mesh syncronization complete");
+	}
+	
+	void PrintSyncLostMessage()
+	{
+		Debug.Log ("Mesh syncronization lost");
+	}
+}
