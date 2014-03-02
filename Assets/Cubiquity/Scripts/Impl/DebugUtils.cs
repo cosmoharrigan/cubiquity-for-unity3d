cb09dad9a0b55f905a77535f6ec51b029660a5ca
David Williams <david@volumesoffun.com>
2014-03-02 10:42:15 +0000

    Added assert function for debugging.

---
+++ Assets/Cubiquity/Scripts/Impl/DebugUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Cubiquity
+{
+	namespace Impl
+	{
+		public class DebugUtils
+		{
+		    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+		    public static void Assert(bool condition, string message)
+		    {
+		        if (!condition) Debug.LogError("Assertion failed: " + message);
+		    }
+		}
+	}
+}
