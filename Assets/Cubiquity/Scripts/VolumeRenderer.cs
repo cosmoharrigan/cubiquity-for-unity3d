740ab5bb4908410d3d902e24cde3a7d1c1869d0e
David Williams <david@volumesoffun.com>
2014-04-22 20:58:03 +0200

    Added VolumeRenderer documentation.

--- Assets/Cubiquity/Scripts/VolumeRenderer.cs
+++ Assets/Cubiquity/Scripts/VolumeRenderer.cs
@@ -5,10 +5,20 @@ using Cubiquity.Impl;
 
 namespace Cubiquity
 {
+	/// Controls some visual aspects of the volume and allows it to be rendered.
+	/**
+	 * The role of the VolumeRenderer component for volumes is conceptually similar to the role of Unity's MeshRenderer class for meshes.
+	 * Specifically, it can be attached to a GameObject which also has a Volume component to cause that Volume component to be drawn. It 
+	 * also exposes a number of properties such as whether a volume should cast and receive shadows.
+	 * 
+	 * Remember that Cubiquity acctually draws the volume by creating standard Mesh objects. Internally Cubiquity will copy the properties
+	 * of the VolumeRenderer to the MeshRenderers which are generated.
+	 */
 	public abstract class VolumeRenderer : MonoBehaviour
 	{
 		public Material material;
 		
+		/// Controls whether this volume casts shadows.
 		public bool castShadows
 		{
 			get
@@ -27,6 +37,7 @@ namespace Cubiquity
 		[SerializeField]
 		private bool mCastShadows;
 		
+		/// Controls whether this volume receives shadows.
 		public bool receiveShadows
 		{
 			get
@@ -45,7 +56,9 @@ namespace Cubiquity
 		[SerializeField]
 		private bool mReceiveShadows;
 		
+		/// \cond
 		public uint lastModified = Clock.timestamp;
+		/// \endcond
 		
 		// Dummy start method rqured for the 'enabled' checkbox to show up in the inspector.
 		void Start() { }
