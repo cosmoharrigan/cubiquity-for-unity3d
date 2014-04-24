3f84acf0a60ea7cb20690c62dbcf4fd31109a05a
David Williams <david@volumesoffun.com>
2014-04-24 23:48:04 +0200

    Work on quick start guide.

--- DocSrc/QuickStart.cs
+++ DocSrc/QuickStart.cs
@@ -4,21 +4,21 @@
  *
  * \section secFirstTerrainVolume Creating your first voxel terrain
  *
- * Cubiquity for Unity3D supports two types of voxel environments. We will begin by looking at the *Terrain Volume* which, as the name suggests, is intended for representing natural terrains. From a user point of view it is similar to Unity3D's built-in terrain, but additionally supports caves, overhangs, and flexible run-time editing.
+ * %Cubiquity for Unity3D supports two types of voxel environments. We will begin by looking at the *Terrain Volume* which, as the name suggests, is intended for representing natural terrains. From a user point of view it is similar to Unity3D's built-in terrain, but additionally supports caves, overhangs, and flexible run-time editing.
  *
- * To create a Terrain Volume from within the unity3D editor you should go to the main menu and select `GameObject -> Create Other -> Terrain Volume`. The terrain volume will be created at the origin of your scene and should appear as shown in the image below:
+ * Begin by opening the scene 'EmptySceneWithLighting' in the 'Assets/Cubiquity/Examples' folder. Now create a Terrain Volume from within the Unity3D editor by going to the main menu and selecting `GameObject -> Create Other -> Terrain Volume`. The terrain volume will be created at the origin of your scene and should appear as shown in the image below:
  * 
- * Picture here...
+ * \image html NewTerrainVolume.jpg "A newly created terrain volume"
  *
  * \subsection secEditing Editing the voxel terrain
  *
- * Your new terrain should be automatically selected, and if you move your mouse cursor over it you should see a light-blue *brush marker* following your cursor position. This indicates where any terrain modification operations will be performed. You can click with the left mouse button to begin adding material into the terrain, thereby creating hills and ridges.
+ * Your new terrain should be automatically selected, and as with any other Unity object you can scale, rotate, and translate it by using the the usual gizmos. To actually edit the terrain you need to select one of the tools from the 'Terrain Volume (Script)' component in the inspector. For example, you could select the 'Sculpt' tool, and then if you move your mouse cursor over the terrain you should see a light-blue *brush marker* following your cursor position. This indicates where any terrain modification operations will be performed. You can click with the left mouse button to begin adding material into the terrain, thereby creating hills and ridges.
  *
- * Then the terrain is selected you will see the editing controls displayed in the Unity3D inspector panel (see picture below). These control the tools which you can use to modify the shape of your terrain or to apply materials to it. You can choose an tool to apply (sculpt, smooth, etc) by selecting one of the buttons at the top of the inspector, and then choose your desired brush options and/or materials. Left-clicking on the terrain will then apply the tool.
+ * \image html SculptTools.png "Terrain volume sculpting tools in the Unity3D editor"
  *
- * Picture of tools here...
+ * Take some time to experiment with the editing tools which are available. You can choose a tool to apply (sculpt, smooth, etc) by selecting one of the buttons at the top of the inspector, and then choose your desired brush options and/or materials. Left-clicking on the terrain will then apply the tool.
  *
- * Take some time to experiment with the editing tools which are available. After you have created a simple terrain you can also try importing one of the standard Unity character controllers so that you can walk around your terrain in play mode. Be aware that it can take a few seconds for the terrain to generate after you press the play button, so for this reason you should set your character to start a hundred units or so above the terrain. This way the terrain will have time to load before the character reaches ground level (there are better approaches, but this is fine for quick-start purposes).
+ * After you have created a simple terrain you can also try importing one of the standard Unity character controllers so that you can walk around your terrain in play mode. Be aware that it can take a few seconds for the terrain to generate after you press the play button, so for this reason you should set your character to start a hundred units or so above the terrain. This way the terrain will have time to load before the character reaches ground level (there are better approaches, but this is fine for quick-start purposes).
  *
  * \section secFirstColoredCubesVolume Creating your first colored cubes volume
  *
