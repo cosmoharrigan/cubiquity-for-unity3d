0e53538180770d4e7f4dd3343ba50d2a1159eca1
David Williams <david@volumesoffun.com>
2014-04-26 09:48:03 +0200

    Fixed section definitions.

--- DocSrc/Principles.cs
+++ DocSrc/Principles.cs
@@ -10,7 +10,7 @@
  *
  * One of the first points to understand is the difference between *Cubiquity* and *Cubiquity for Unity3D*. The first of these, *Cubiquity*, is a native code (i.e. C/C++) library for storing, editing, and rendering voxel worlds. It is not tied to any particular game engine or platform, and can be used from a variety of languages. On the other hand, *Cubiquity for Unity3D* is a set of C# scripts which connect %Cubiquity to the Unity3D game engine. These scripts allow Unity3D games to create, edit and display Cubiquity volumes. As a user of %Cubiquity for Unity3D you do not (normally) have access to the underlying C/C++ code of %Cubiquity, but you do have a compiled version of the %Cubiquity library and the source code to the C# integration scripts.
  *
- * \section Voxel Engine Concepts
+ * \section secConcepts Voxel Engine Concepts
  *
  * Cubiquity is a *voxel engine*, which means that it represents it's objects as samples on a 3D grid. This is conceptually similar to the way a bitmap image represents a photograph as samples on a 2D grid (the pixels). In many ways a voxel can be considered the 3D equivalent of a pixel. The value stored at a voxel may simply be a colour (as in the case in our ColoredCubesVolume) or it may be some more advanced representation.
  *
@@ -18,7 +18,7 @@
  *
  * %Cubiquity largely hides this implementation detail from the user. All you need to do is write the desired values into the voxels, and %Cubiquity automatically takes care of keeping the corresponding mesh synchronized with them. These voxel values may be read from disk, be generated procedurally, or created through other approaches. This is largely up to you.
  *
- * \section Understanding Cubiquity
+ * \section secCubiquity Understanding Cubiquity
  *
  * In general you should not need to know much about the underlying %Cubiquity engine as most important details are hidden behind the C# scripts, but this section discusses a couple of details which are worth keeping in mind.
  *
@@ -32,7 +32,7 @@
  *
  * Voxel environments can get very large (potentially containing billions of voxels) so efficient storage of such worlds is of utmost importance. The Cubiquity voxel engine stores a volume as a *Voxel Database*, which is a single file containing all the voxels in the volume. Internally it is actually an <a href="http://www.sqlite.org">SQLite</a> database and so can be opened with a tool such as <a href="http://sqlitebrowser.sourceforge.net/">SQLite Browser</a>. Such as tool will let you view certain properties of the volume such as its dimensions, but you won't be able to gain any meaningful insight into the voxel data itself as it is stored in an efficient compressed format.
  * 
- * \section Understanding Cubiquity for Unity3D
+ * \section secCubiquityForUnity3D Understanding Cubiquity for Unity3D
  *
  * As mentioned previously, *Cubiquity for Unity3D* is the integration layer which connects our Cubiquity voxel engine to Unity3D. It does this by wrapping voxel databases as Unity3D assets, converting the generated mesh to the the required format, and providing Unity3D materials to control the appearance of your voxel objects. These and other aspects of the system are discussed in this section.
  *
@@ -42,21 +42,21 @@
  *
  * Note that the components shown are actually the base classes and in practice are never used directly. Instead you will use a particular set of subclasses (such as TerrainVolume, TerrainVolumeData, TerrainVolumeRenderer and TerrainVolumeCollider) depending on the kind of volume you would like to create. We now look at each of these components in more detail.
  *
- * \subsection The Volume Component
+ * \subsection secVolComp The Volume Component
  *
  * Adding a Volume component to a GameObject makes it into a voxel object. The Volume class does not provide much functionality on its own as most behaviour (rendering, physics, etc) is implemented in the other related components. Instead the class acts more to tie the other components together.
  *
  * Volume components also have custom inspector implemented which allow you to edit the volume in an intuitive way. For example, the TerrainVolume has a custom inspector which exposes the sculpting and painting tools which can be used to build your own terrain in the Unity3D editor. The inspector also has a 'Settings' panel where you can choose which volume data is being used.
  *
- * \subsection The VolumeRenderer Component
+ * \subsection secVolRendComp The VolumeRenderer Component
  *
  * The VolumeRenderer Component...
  *
- * \subsection The VolumeCollider Component
+ * \subsection secVolColComp The VolumeCollider Component
  *
  * The VolumeCollider Component...
  *
- * \subsection The VolumeData Component and Cubiquity Assets
+ * \subsection secVolDatComp The VolumeData Component and Cubiquity Assets
  *
  * Cubiquity for Unity3D wraps voxel databases with a class called VolumeData, and more specifically with its subclasses called TerrainVolumeData and ColoredCubesVolumeData. These are very thin wrappers, which basically just store the filename of the voxel database and provide functions to get and set the voxel values. 
  */
