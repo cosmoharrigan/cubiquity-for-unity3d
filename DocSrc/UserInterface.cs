ce32f7fe2c73edbda3962e771deba947840c1a5c
David Williams <david@volumesoffun.com>
2014-04-26 21:51:01 +0200

    Started working on instructions for user interface.

--- DocSrc/UserInterface.cs
+++ DocSrc/UserInterface.cs
@@ -1,3 +1,17 @@
-/** \page pageUserInterface User Interface 
+/** \page pageUserInterface User Interface
+ *
+  * %Cubiquity for Unity3D is tightly coupled with the Unity3D editor and strives to follow similar design and usage approaches to those used in other parts of Unity. This means that concepts such as component-based design, assets, inspectors, transform gizmos, and materials should all work in similar way to those which you are used to, and the interface to the TerrainVolume also takes significant cues from Unity's standard terrain.
+ *
+ * We hope that you will therefore find the system intuitive, but none-the-less we use this section of the manual to outline the key user interface elements and how they fit together.
+ *
+ * \section secAddVolumes Adding Volumes To The Scene
+ * \section secTransVolumes Transforming Volumes
+ * \section secEditVolumes Editing Volumes
+ * \subsection secEditTerrainVolumes Editing Terrain Volumes
+ * \subsection secEditColCubesVolumes Editing Colored Cubes Volumes
+ * \section secOtherComps Other Volume Components
+ * \subsection secRendererComp The Volume Renderer
+ * \subsection secColliderComp The Volume Collider
+ * \section Creating New Volume Data And Assets
  *
  */
