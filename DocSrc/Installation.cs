820f21dd55cf52cf599ab55f0eca6473c98493ed
David Williams <david@volumesoffun.com>
2014-04-28 21:30:39 +0200

    Work on documentation.

--- DocSrc/Installation.cs
+++ DocSrc/Installation.cs
@@ -1,15 +1,19 @@
 /** \page pageInstallation Installation
  * 
- * There are currently two ways of obtaining Cubiquity for Unity3D. You can either download and install a '.unitypackage' file (available on the Asset Store or from our website), or get the latest development version from our git repository.
+ * There are currently three ways of obtaining %Cubiquity for Unity3D. You can install it from the asset store if you are purchasing the commercial edition of %Cubiquity, you can download a free-standing '.unitypackage' file if you are using non-commercial version of %Cubiquity, or you can get the latest development version from our git repository.
+ *
+ * \section secInstallFromAssetStore Installing From The Asset Store
+ *
+ * If you purchase the commercial version of %Cubiquity for Unity3D then you can download and install it directly though the asset store, as with any other asset store purchase. You also have the option to download the .unitypackage file for later installation, in which case you should follow the instructions below.
  *
  * \section secInstallFromPackage Installing a Package File
  *
- * When purchasing Cubiquity for Unity3D on the Asset Store you are immediately provided with an option to install. If you instead choose to save the package file for later use, or if you choose to download the non-commercial version of Cubiquity for Unity3D from our website, then you need to import the package file by going to the main menu in the Unity3D editor and selecting `Assets -> Import Package -> Custom Package..` and then locating your downloaded package file. The unity package importer will then present you with a list of files which you can choose to import - for now the best option is to make sure everything is selected and then press `Import`.
+ * If you choose to download the non-commercial version of %Cubiquity for Unity3D from then you need to import the package file. You can do this by going to the main menu in the Unity3D editor and selecting `Assets -> Import Package -> Custom Package...` and then locating your downloaded package file. The unity package importer will then present you with a list of files which you can choose to import - for now the best option is to make sure everything is selected and then press `Import`.
  *
  * The process we have described here is standard for all Unity3D packages, so if you have any difficulties you should consult the Unity3D documentation.
  *
  * \section secInstallFromGit Installing from Git
  *
- * Advanced users may be interested in using the latest development version of Cubiquity for Unity3D, rather than the stable releases. By doing this you may get access to functionality before it is officially released, but you can should expect that this is not yet fully tested. If you are interested in this then you can find our Git repository at https://bitbucket.org/volumesoffun/cubiquity-for-unity3d where further instructions are also provided.
+ * Advanced users may be interested in using the latest development version of %Cubiquity for Unity3D, rather than the stable releases. By doing this you may get access to functionality before it is officially released, but you can should expect that this is not yet fully tested. If you are interested in this then you can find our Git repository at <a href="http://bitbucket.org/volumesoffun/cubiquity-for-unity3d"><b>http://bitbucket.org/volumesoffun/cubiquity-for-unity3d</b></a> where further instructions are also provided.
  *
  */
