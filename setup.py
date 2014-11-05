d66b0d8650a316ac26bbdda18ee70bb2a318c06c
David Williams <david@volumesoffun.com>
2014-11-05 12:27:10 +0100

    Removed setup.py that was added for testing - it didn't seem to help.

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distribute_setup import use_setuptools
-use_setuptools() 
-
-import sys
-from setuptools import setup, find_packages
-from requirements_utils import parse_dependency_links, parse_requirements
